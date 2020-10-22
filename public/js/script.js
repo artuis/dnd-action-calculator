49fc21b1335053562af7b08975f47b8daaae5639
Thomas An <tommyan97@msn.com>
2020-10-22 10:01:52 -0700

    added basic post to save character

--- public/js/script.js
+++ public/js/script.js
@@ -19,7 +19,29 @@ $(document).ready(function() {
     });
   
     $("#save-character").click(() => {
-      console.log("hello");
+      const newChar = {
+        name : $("#character-name").val().trim(),
+        player_name : $("#player-name").val().trim(),
+        experience : parseInt($("#exp").val().trim()),
+        level : parseInt($("#level").val().trim()),
+        strength : parseInt($("#str").val().trim()),
+        dexterity : parseInt($("#dex").val().trim()),
+        constitution : parseInt($("#con").val().trim()),
+        intelligence : parseInt($("#int").val().trim()),
+        wisdom : parseInt($("#wis").val().trim()),
+        charisma : parseInt($("#cha").val().trim()),
+        perception : modifier(parseInt($("#wis").val().trim())),
+        initiative : parseInt($("#init").val().trim()),
+        hp_current : parseInt($("#hp").val().trim()),
+        hp_temp : parseInt($("#hp").val().trim()),
+        hp_max : parseInt($("#hp").val().trim()),
+        arnor_class : parseInt($("#ac").val().trim()),
+        shield : false
+      }
+
+      $.post("/api/characters", newChar, data => {
+        console.log(data);
+      })
     })
   
   
@@ -34,3 +56,8 @@ $(document).ready(function() {
   
   
   });
+
+// converts ability score to modifier
+const modifier = (stat) => {
+    Math.floor((stat - 10) / 2)
+}
