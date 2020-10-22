0c5e45e95f98d99c942a1f6ce09781b9dc677de7
Thomas An <tommyan97@msn.com>
2020-10-22 11:16:54 -0700

    changed htmlroutes to route to index when GET /

--- controllers/htmlroutes.js
+++ controllers/htmlroutes.js
@@ -4,18 +4,15 @@ module.exports = function(app) {
     const characters2 = [{id: 1, name : "Player 5"}, {id: 2, name : "Player 6"}, {id: 3, name : "Player 7"}, {id: 4, name : "Player 8"}];
     const characters3 = [{id: 1, name : "Player 9"}, {id: 2, name : "Player 10"}, {id: 3, name : "Player 11"}, {id: 4, name : "Player 12"}];
     const data = [{id : 1, name : "Campaign 1", characters : characters1}, {id : 2, name : "Campaign 2", characters : characters2},{id : 3, name : "Campaign 3", characters : characters3}]
-    app.get("/", function(req, res) {
-        console.log("GET home page")
-        res.render("testing");
+    app.get("/", (req, res) => {
+        console.log("GET index page")
+        res.render("index");
     });
     app.get("/campaigns", (req, res) => {
         console.log("GET campaign page")
         res.render("campaigns", { campaigns: data });
     });
-    app.get("/index", (req, res) => {
-        console.log("GET index page")
-        res.render("index");
-    });
+    
     app.get("/login", (req, res) => {
         console.log("GET login page")
         res.render("login");
