06b9d0696eb048519a2e32c45ee3eaac84b53d0d
Magnus Appel <68828420+magnuss86@users.noreply.github.com>
2020-10-23 13:48:07 -0700

    fixed nodemailer

--- controllers/mailercontroller.js
+++ controllers/mailercontroller.js
@@ -1,29 +1,34 @@
-// const express = require("express");
-// const router = express.Router();
-// const db = require("../models")
-// var nodemailer = require('nodemailer');
+const express = require("express");
+const router = express.Router();
+const db = require("../models")
+var nodemailer = require('nodemailer');
 
-// var transporter = nodemailer.createTransport({
-//   service: 'gmail',
-//   auth: {
-//     user: 'dndcompanionapp@gmail.com',
-//     pass: 'Nodemail20'
-//   }
-// });
+function sendEmail(email) {
+    
 
-// var mailOptions = {
-//   from: 'dndcompanionapp@gmail.com',
-//   to: 'dndcompanionapp@gmail.com',
-//   subject: 'Your account has been successfully created!',
-//   text: 'Thank you for creating an aaccount. Your login is:   and ypur account can be accessed at any time'
-// };
 
-// transporter.sendMail(mailOptions, function(error, info){
-//   if (error) {
-//     console.log(error);
-//   } else {
-//     console.log('Email sent: ' + info.response);
-//   }
-// });
+var transporter = nodemailer.createTransport({
+  service: 'gmail',
+  auth: {
+    user: 'dndcompanionapp@gmail.com',
+    pass: 'Nodemail20'
+  }
+});
 
-// module.exports = mailer;
+var mailOptions = {
+  from: 'dndcompanionapp@gmail.com',
+  to: `${email}`,
+  subject: 'Your account has been successfully created!',
+  text: 'Thank you for creating an account! Please enjoy making a campagin and creating characters!'
+};
+
+transporter.sendMail(mailOptions, function(error, info){
+  if (error) {
+    console.log(error);
+  } else {
+    console.log('Email sent: ' + info.response);
+  }
+});
+}
+
+module.exports = sendEmail;
