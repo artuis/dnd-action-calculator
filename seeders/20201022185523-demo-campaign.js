bce33ec42fffd73b12b5d961a52ce1f1b7b4e6c4
Thomas An <tommyan97@msn.com>
2020-10-23 10:57:18 -0700

    modified campaigns render to render database

--- seeders/20201022185523-demo-campaign.js
+++ seeders/20201022185523-demo-campaign.js
@@ -3,11 +3,27 @@
 module.exports = {
   up: async (queryInterface, Sequelize) => {
     return queryInterface.bulkInsert('Campaigns', [{
-      name: "exampleCampaign",
+      name: "exampleCampaign1",
       AccountId: 1,
       createdAt: new Date(),
       updatedAt: new Date()
-    }])
+    },{
+      name: "exampleCampaign2",
+      AccountId: 1,
+      createdAt: new Date(),
+      updatedAt: new Date()
+    },{
+      name: "exampleCampaign3",
+      AccountId: 1,
+      createdAt: new Date(),
+      updatedAt: new Date()
+    },{
+      name: "exampleCampaign4",
+      AccountId: 1,
+      createdAt: new Date(),
+      updatedAt: new Date()
+    }
+  ])
   },
 
   down: async (queryInterface, Sequelize) => {
